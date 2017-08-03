337f86f35a1b8a7546233375b336692823b0e5a0
airodrigues <airodrigues@hotmail.com>
2017-08-03 16:19:32 -0300

    Initial commit

---
+++ android/src/rmit/com/br/flappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package rmit.com.br.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import rmit.com.br.flappybird.FlappyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBird(), config);
+	}
+}
